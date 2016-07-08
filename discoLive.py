df4f7971f73c74f27bff69fa81f732db1e623c4c
Anant Shah <akshah@cs.colostate.edu>
2016-07-08 15:06:07 +0900

    Output to file

--- discoLive.py
+++ discoLive.py
@@ -43,8 +43,11 @@ def on_result_response(*args):
     #print args[0]
     item=args[0]
     event = eval(str(item))
+    print(event,flush=True)
     if event["event"] == "disconnect":
-        dataQueue.put(event)
+        dataQueueDisconnect.put(event)
+    elif event["event"] == "connect":
+        dataQueueConnect.put(event)
 
 def getCleanVal(val,tsClean):
     newVal=val+1
@@ -52,42 +55,45 @@ def getCleanVal(val,tsClean):
         newVal=val+1
     return newVal
 
-def worker():
+def workerD():
     while True:
         eventLocal=[]
         eventClean=[]
         tsClean=[]
         stateAvgBurstRateDict={}
-        waitTime=(30*60)/30
-        print('----------')
-        print('Thread waiting for {0} seconds...'.format(waitTime))
+        waitTime=(5*60)
+        #print('----------')
+        #print('Thread waiting for {0} seconds...'.format(waitTime))
         time.sleep(waitTime)
-        #if dataQueue.qsize() > QUEUE_THRESHOLD:
-            #while dataQueue.qsize() > QUEUE_THRESHOLD:
+        #if dataQueueDisconnect.qsize() > QUEUE_THRESHOLD:
+            #while dataQueueDisconnect.qsize() > QUEUE_THRESHOLD:
 
-        itemsToRead=dataQueue.qsize()
-        if itemsToRead>1:
-            print('Items in queue: {0}'.format(itemsToRead))
+        itemsToRead=dataQueueDisconnect.qsize()
+        if itemsToRead>=1:
+            #print('Items in queue: {0}'.format(itemsToRead))
             while itemsToRead:
-                event=dataQueue.get()
+                event=dataQueueDisconnect.get()
                 #eventTimestamp=event['timestamp']
                 #eventProbeID=event['id']
+                #print(eventProbeID)
                 eventLocal.append(event)
                 itemsToRead-=1
 
             #Manage duplicate values
             for eventVal in eventLocal:
-                newVal=eventVal['timestamp']
-                if newVal in tsClean:
-                    newVal=getCleanVal(newVal,tsClean)
+                newVal=int(str(eventVal['timestamp'])+'00')
+                #print('Detected overlapping timestamps, will flatten data')
+                while newVal in tsClean:
+                    newVal+=1
+                #print('Fixed')
                 tsClean.append(newVal)
                 eventVal['timestamp']=newVal
                 eventClean.append(eventVal)
 
             tsClean.sort()
             bursts = kleinberg(tsClean)
-            plotBursts(bursts)
-            print('Bursts: {0}'.format(bursts))
+            #plotBursts(bursts)
+            #print('Bursts: {0}'.format(bursts))
             burstsDict={}
             for brt in bursts:
                 #print(brt)
@@ -100,20 +106,22 @@ def worker():
                 burstsDict[q]['end']=qend
             for state,timesDict in burstsDict.items():
                 try:
-                    print('State {0}:'.format(state))
-                    print('Start time: {0}'.format(timesDict['start']))
-                    print('End time: {0}'.format(timesDict['end']))
-                    print('Probes:')
+                    #print('State {0}:'.format(state))
+                    #print('Start time: {0}'.format(timesDict['start']))
+                    #print('End time: {0}'.format(timesDict['end']))
+                    #print('Probes:')
                     eventCounter=0
                     for evnt in eventClean:
                         if evnt['timestamp']>= timesDict['start'] and evnt['timestamp']<= timesDict['end']:
                             eventCounter+=1
-                            print(' Probe ID: {0}'.format(evnt['id']))
-                            print(' Prefix: {0}'.format(evnt['prefix']))
-                            print(' Country: {0}'.format(evnt['country_code']))
-                            print(' ASN: {0}'.format(evnt['asn']))
-                    burstRate=eventCounter/(timesDict['end']-timesDict['start'])
-                    print('Average Burst Rate: {0}'.format(burstRate))
+                            #print(' Probe ID: {0}'.format(evnt['id']))
+                            #print(' Prefix: {0}'.format(evnt['prefix']))
+                            #print(' Country: {0}'.format(evnt['country_code']))
+                            #print(' ASN: {0}'.format(evnt['asn']))
+                    burstRate='-1'
+                    if timesDict['end']!=timesDict['start']:
+                        burstRate=eventCounter/(timesDict['end']-timesDict['start'])
+                        #print('Average Burst Rate: {0}'.format(burstRate))
                     stateAvgBurstRateDict[state]=burstRate
                 except KeyError:
                     pass
@@ -121,24 +129,106 @@ def worker():
                 except:
                     traceback.print_exc()
             maxState=max(stateAvgBurstRateDict.keys(), key=int)
-            output.write([maxState,stateAvgBurstRateDict[maxState]])
-            print('----------')
-            dataQueue.task_done()
+            output.write([maxState,round(float(stateAvgBurstRateDict[maxState]),5)])
+            #print('----------')
+
+            dataQueueDisconnect.task_done()
+
+def workerC():
+    while True:
+        eventLocal=[]
+        eventClean=[]
+        tsClean=[]
+        stateAvgBurstRateDict={}
+        waitTime=(5*60)
+        #print('----------')
+        #print('Thread waiting for {0} seconds...'.format(waitTime))
+        time.sleep(waitTime)
+        #if dataQueueConnect.qsize() > QUEUE_THRESHOLD:
+            #while dataQueueConnect.qsize() > QUEUE_THRESHOLD:
+
+        itemsToRead=dataQueueConnect.qsize()
+        if itemsToRead>=1:
+            #print('Items in queue: {0}'.format(itemsToRead))
+            while itemsToRead:
+                event=dataQueueConnect.get()
+                #eventTimestamp=event['timestamp']
+                #eventProbeID=event['id']
+                #print(eventProbeID)
+                eventLocal.append(event)
+                itemsToRead-=1
+
+            #Manage duplicate values
+            for eventVal in eventLocal:
+                newVal=int(str(eventVal['timestamp'])+'00')
+                #print('Detected overlapping timestamps, will flatten data')
+                while newVal in tsClean:
+                    newVal+=1
+                #print('Fixed')
+                tsClean.append(newVal)
+                eventVal['timestamp']=newVal
+                eventClean.append(eventVal)
+
+            tsClean.sort()
+            bursts = kleinberg(tsClean)
+            #plotBursts(bursts)
+            #print('Bursts: {0}'.format(bursts))
+            burstsDict={}
+            for brt in bursts:
+                #print(brt)
+                q=brt[0]
+                qstart=brt[1]
+                qend=brt[2]
+                if q not in burstsDict.keys():
+                    burstsDict[q]={}
+                burstsDict[q]['start']=qstart
+                burstsDict[q]['end']=qend
+            for state,timesDict in burstsDict.items():
+                try:
+                    #print('State {0}:'.format(state))
+                    #print('Start time: {0}'.format(timesDict['start']))
+                    #print('End time: {0}'.format(timesDict['end']))
+                    #print('Probes:')
+                    eventCounter=0
+                    for evnt in eventClean:
+                        if evnt['timestamp']>= timesDict['start'] and evnt['timestamp']<= timesDict['end']:
+                            eventCounter+=1
+                            #print(' Probe ID: {0}'.format(evnt['id']))
+                            #print(' Prefix: {0}'.format(evnt['prefix']))
+                            #print(' Country: {0}'.format(evnt['country_code']))
+                            #print(' ASN: {0}'.format(evnt['asn']))
+                    burstRate='-1'
+                    if timesDict['end']!=timesDict['start']:
+                        burstRate=eventCounter/(timesDict['end']-timesDict['start'])
+                        #print('Average Burst Rate: {0}'.format(burstRate))
+                    stateAvgBurstRateDict[state]=burstRate
+                except KeyError:
+                    pass
+                    #print(evnt)
+                except:
+                    traceback.print_exc()
+            maxState=max(stateAvgBurstRateDict.keys(), key=int)
+            output2.write([maxState,round(float(stateAvgBurstRateDict[maxState]),5)])
+            #print('----------')
+
+            dataQueueConnect.task_done()
+
 
 def kleinberg(data, verbose=5):
     # make timestamps relative to the first one
     ts = np.array(data)
 
-    print('Performing Kleinberg burst detection..')
-    bursts =  pybursts.kleinberg(ts, s=5, gamma=0.3)
+    #print('Performing Kleinberg burst detection..')
+    bursts = pybursts.kleinberg(ts, s=3, gamma=2)
 
+    '''
     # Give dates of prominent bursts
     if verbose is not None:
         for q, ts, te in bursts:
             if q >= verbose:
-                print "Burst level %s from %s to %s." % (q, dt.datetime.utcfromtimestamp(ts/10),
-                        dt.datetime.utcfromtimestamp(te/10))
-
+                print "Burst level %s from %s to %s." % (q, dt.datetime.utcfromtimestamp(ts),
+                        dt.datetime.utcfromtimestamp(te))
+    '''
     return bursts
 
 def plotBursts(bursts):
@@ -169,25 +259,39 @@ def plotBursts(bursts):
 if __name__ == "__main__":
     QUEUE_THRESHOLD=2
     ts = []
-    dataQueue=Queue.Queue()
+    dataQueueDisconnect=Queue.Queue()
+    dataQueueConnect=Queue.Queue()
     output=outputWriter(resultfilename='discoResults.csv')
+    output2=outputWriter(resultfilename='discoResults2.csv')
     for i in range(0,1):
-        t = threading.Thread(target=worker)
+        t = threading.Thread(target=workerD)
+        t.daemon = True  # Thread dies when main thread (only non-daemon thread) exits.
+        t.start()
+    for i in range(0,1):
+        t = threading.Thread(target=workerC)
         t.daemon = True  # Thread dies when main thread (only non-daemon thread) exits.
         t.start()
 
-    #Read Stream
-    atlas_stream = AtlasStream()
-    atlas_stream.connect()
+    try:
+        #Read Stream
+        atlas_stream = AtlasStream()
+        atlas_stream.connect()
 
-    # Probe's connection status results
-    channel = "probe"
-    atlas_stream.bind_channel(channel, on_result_response)
-    #1409137200
-    stream_parameters = {"enrichProbes": True,"startTime":1409132340,"endTime":1409132440,"speed":30}
-    atlas_stream.start_stream(stream_type="probestatus", **stream_parameters)
+        # Probe's connection status results
+        channel = "probe"
 
-    atlas_stream.timeout()
-    dataQueue.join()
-    # Shut down everything
-    atlas_stream.disconnect()
+        atlas_stream.bind_channel(channel, on_result_response)
+        #1409132340
+        #1409137200
+        stream_parameters = {"startTime":1409132240,"endTime":1409137200,"speed":5}
+        #stream_parameters = {"enrichProbes": True}
+        atlas_stream.start_stream(stream_type="probestatus", **stream_parameters)
+
+        atlas_stream.timeout()
+        dataQueueDisconnect.join()
+        dataQueueConnect.join()
+        # Shut down everything
+        atlas_stream.disconnect()
+    except:
+        print('Unexpected Event. Quiting.')
+        atlas_stream.disconnect()
--- plotABR.py
+++ plotABR.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
 import csv
+import time
 
-abrList=[]
-with open('discoResults.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter='|')
-    for row in reader:
-        abrList.append(float(row[1]))
+plt.ion()
+plt.plot([], 'c',label='Disconnections',lw=3)
+plt.plot([], 'g',label='Connections',lw=3)
+plt.legend()
+while True:
+    abrListD=[]
+    with open('discoResults.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter='|')
+        for row in reader:
+            abrListD.append(float(row[1]))
 
-plt.plot(abrList, 'c')
-plt.ylim(0,max(abrList)+0.5)
-plt.show()
+    abrListC=[]
+    with open('discoResults2.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter='|')
+        for row in reader:
+            abrListC.append(float(row[1]))
+
+    plt.plot(abrListD, 'c',label='Disconnections',lw=3)
+    plt.plot(abrListC, 'g',label='Connections',lw=3)
+    minVal=min(min(abrListD),min(abrListC))
+    maxVal=max(max(abrListD),max(abrListC))
+    #plt.ylim(minVal-0.05,maxVal+0.05)
+    plt.pause(1)
